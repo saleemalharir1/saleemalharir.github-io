12f16e3fcd69e5428e37980f167a05defe7c8013
Saleem Alharir <saleem.alharir@utah.edu>
2023-03-27 21:04:59 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// Add your JavaScript code here
+<script>
+  const toggleBtn = document.querySelector('#toggle-btn');
+  const body = document.querySelector('body');
+
+  toggleBtn.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+  });
+</script>
